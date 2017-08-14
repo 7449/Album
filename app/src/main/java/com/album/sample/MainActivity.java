448de8d85559280b27da5eeda4f24e35c7eeff41
@ <yuezhaoyang7449@163.com>
2017-08-14 15:38:02 +0800

    commit first project

---
+++ app/src/main/java/com/album/sample/MainActivity.java
@@ -0,0 +1,14 @@
+package com.album.sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

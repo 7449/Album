9b77eeddd8c04d4e54197084e6b54cb7a0a0b1ac
@ <yuezhaoyang7449@163.com>
2017-09-04 15:33:40 +0800

    add scanView CursorLoader sample

--- AlbumLibrary/src/main/java/com/album/ui/view/ScanView.java
+++ AlbumLibrary/src/main/java/com/album/ui/view/ScanView.java
@@ -11,10 +11,39 @@ import java.util.ArrayList;
 
 /**
  * by y on 17/08/2017.
+ * <p>
+ * <p>
+ * //        final Activity albumActivity = activity;
+ * //
+ * //        LoaderManager loaderManager = albumActivity.getLoaderManager();
+ * //        final Bundle bundle = new Bundle();
+ * //        bundle.putStringArray("albumKey", new String[]{bucketId, String.valueOf(page), String.valueOf(count)});
+ * //        loaderManager.initLoader(0, bundle, new LoaderManager.LoaderCallbacks<Cursor>() {
+ * //        @Override
+ * //        public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+ * //            String[] albumArgs = bundle.getStringArray("albumKey");
+ * //            return new CursorLoader(albumActivity,
+ * //                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+ * //                    null,
+ * //                    null,
+ * //                    null,
+ * //                    null);
+ * //        }
+ * //
+ * //        @Override
+ * //        public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+ * //            AlbumTool.log(data.getCount());
+ * //        }
+ * //
+ * //        @Override
+ * //        public void onLoaderReset(Loader<Cursor> loader) {
+ * //
+ * //        }
+ * //    });
  */
 
-public interface ScanView {
 
+public interface ScanView {
     void start(ContentResolver contentResolver, ScanCallBack scanCallBack, String bucketId, int page, int count);
 
     void resultScan(ContentResolver contentResolver, ScanCallBack scanCallBack, String path);

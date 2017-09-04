1c279f2a72b99bec1c5ea04c155b815a6189d5b3
@ <yuezhaoyang7449@163.com>
2017-09-04 15:25:48 +0800

    delete

--- AlbumLibrary/src/main/java/com/album/presenter/AlbumPresenter.java
+++ AlbumLibrary/src/main/java/com/album/presenter/AlbumPresenter.java
@@ -1,7 +1,6 @@
 package com.album.presenter;
 
 import com.album.model.AlbumModel;
-import com.album.ui.widget.ScanCallBack;
 
 import java.util.ArrayList;
 
@@ -9,7 +8,7 @@ import java.util.ArrayList;
  * by y on 14/08/2017.
  */
 
-public interface AlbumPresenter extends ScanCallBack {
+public interface AlbumPresenter {
     void scan(String bucketId, int page, int count);
 
     void mergeModel(ArrayList<AlbumModel> albumList, ArrayList<AlbumModel> multiplePreviewList);
